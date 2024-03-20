08c574692408d087c7259316ca9d0f2e8f50bc87
evarellapucky <scherer.alexia@gmail.com>
2024-03-20 15:52:55 +0100

    form sign in / sign up + temporary navbar

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   config.action_controller.raise_on_missing_callback_actions = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.assets.debug = true
 end
