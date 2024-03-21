caa65ecc50753151198f383eebcff6bda00035e0
evarellapucky <scherer.alexia@gmail.com>
2024-03-21 15:54:26 +0100

    deploy v2 + mails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 
   def welcome_email(user, password)
     @user = user
-    @url = "https://lamaisondessons.fy.dev/users/sign_in"
+    @url = "https://la-maison-des-sons.fly.dev/users/sign_in"
     @password = password
 
     mail(to: @user.email, subject: "Bienvenue à la Maison des sons !")
