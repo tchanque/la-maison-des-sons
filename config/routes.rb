0971d2b2cab1898886d96299c30e38142f19800e
Marcaraph <ra.marcarini@gmail.com>
2024-03-22 14:21:41 +0100

    Update button, switch image, fix dimensions card & redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   resources :users, only: [:show, :destroy]
   resources :events
   resources :attendances, only: [:create, :destroy]
