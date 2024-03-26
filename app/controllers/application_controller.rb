73fd9bb1e017e233ff266efe018fe6bb4bfd75a7
evarellapucky <scherer.alexia@gmail.com>
2024-03-26 17:28:43 +0100

    adding a new column end_date to user + mod credentials + mod admin dashboard + testing status change with end_date (does not work yet)
    Co-authored-by: Marcaraph <Marcaraph@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,14 @@
 class ApplicationController < ActionController::Base
   # # MEANS THE FIRST ACTION FOR THE USER WHEN HE ARRIVES ON THE APP IS TO LOG IN
   before_action :authenticate_user!
-
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
   protected
   # HERE WE ADD SOME PERMISSION TO DEVISE TO UPDATE USER_PROFILE. Because devise accept only email, password and password_confirmation by default
     def configure_permitted_parameters
-         devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :description, :password, :current_password, :password_confirmation)}
+         devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :description, :password, :current_password, :password_confirmation, :end_date, :is_subscriber, :is_teacher, :is_admin)}
          devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :is_admin, :is_teacher, :is_subscriber, :description, :password, :password_confirmation)}
     end
+
+  
 end
