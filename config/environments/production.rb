22b316fa6c9f6efc6748672434fdcf951152203f
evarellapucky <scherer.alexia@gmail.com>
2024-03-27 23:45:53 +0100

    production config action mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,6 +94,7 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.default_url_options = { host: 'https://la-maison-des-sons.fly.dev/' }
 
   ActionMailer::Base.smtp_settings = {
     :user_name => ENV['MAILJET_LOGIN'],
