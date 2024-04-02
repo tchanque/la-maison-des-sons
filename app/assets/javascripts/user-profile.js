921d7d1f083c572b749d1eba2d9d34241d0b4c4e
tchanque <audevie.thomas@gmail.com>
2024-04-02 11:49:04 +0200

    hover effect and click on avatar to change image

--- app/assets/javascripts/user-profile.js
+++ app/assets/javascripts/user-profile.js
@@ -1,14 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var avatarImage = document.getElementById("avatar_image");
   var avatarInput = document.getElementById("avatar_input");
+  var avatarWrap = document.getElementById("img_wrap");
 
   // Trigger file input click when image is clicked
-  avatarImage.addEventListener("click", function() {
+  avatarWrap.addEventListener("click", function() {
     avatarInput.click();
   });
 
-  // Show the form when a file is selected
-  avatarInput.addEventListener("change", function() {
-    document.getElementById("avatar_form").style.display = "block";
-  });
 });
